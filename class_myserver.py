5e616ae0cfaff577c4ffc3c61af74dd84b91bf57
Fryskin <gman358325@gmail.com>
2023-07-05 23:34:19 +0200

    Added new stuff to project.

--- class_myserver.py
+++ class_myserver.py
@@ -12,24 +12,10 @@ class MyServer(BaseHTTPRequestHandler):
             html_code = html_file.read()
         return f"""{html_code}"""
 
-    def __get_article_content(self, page_address):
-        pass
-
-    def __get_blog_article(self, page_address):
-        return f"""
-        <html><head><title>Blog</title></head><body>
-        <a href="/">Back</a><br>
-        <p>{self.__get_article_content(page_address)}</p>
-        </body>
-        </html>
-        """
-
     def do_GET(self):
         query_components = parse_qs(urlparse(self.path).query)
-        page_address = query_components.get('page')
+        print(query_components)
         page_content = self.__get_index()
-        if page_address:
-            page_content = self.__get_blog_article(page_address[0])
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
